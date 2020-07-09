a6c28b1a06f7b417efad4ed40b857be5a6eed2c5
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-07-09 09:14:10 -0300

    Basic fire data

--- fire.js
+++ fire.js
@@ -1,9 +1,17 @@
-function start() {
+const firePixelsArray = [];
+const fireWidth = 2;
+const fireHeight = 3;
 
+function start() {
+    createFireDataStructure()
 }
 
 function createFireDataStructure() {
+    const numberOfPixels = fireWidth * fireHeight;
 
+    for (let i = 0; i < numberOfPixels; i ++){
+        firePixelsArray[i] = 0;
+    }
 }
 
 function calculateFirePropagation() {
@@ -11,7 +19,7 @@ function calculateFirePropagation() {
 }
 
 function renderFire() {
-    
+
 }
 
 start()
